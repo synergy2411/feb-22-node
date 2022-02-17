b86af40a92873b6ac42b6b18f0cd726813481a58
Sumit K <synergy2411@outlook.com>
2022-02-17 12:01:30 +0530

    REST created

---
+++ 05-express-demo/src/index.js
@@ -0,0 +1,85 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+const users = [
+  {
+    id: "10001",
+    email: "foo@test.com",
+    age: 32,
+  },
+  {
+    id: "10002",
+    email: "bar@test.com",
+    age: 34,
+  },
+  {
+    id: "10003",
+    email: "baz@test.com",
+    age: 36,
+  },
+];
+
+
+app.patch("/api/users/:userId", (req, res)=>{
+    const { userId } = req.params;
+    const position = users.findIndex(user => user.id === userId)
+    if(position >= 0){
+        const newUser = {...users[position], ...req.body}
+        users[position] = newUser;
+        return res.send(newUser);
+    }else{
+        return res.send({err : "Unable to find user to delete"})
+    }
+})
+
+app.delete("/api/users/:userId", (req, res) => {
+    const { userId } = req.params;
+    const foundUser = users.find(user => user.id === userId)
+    if(foundUser){
+        const position = users.findIndex(user => user.id === userId)
+        const deletedUser = users.splice(position, 1)
+        return res.send(deletedUser)
+    }else{
+        return res.send({err : "Unable to find user to delete"})
+    }
+})
+
+// http://localhost:9091/api/users - POST - create new user - request body
+app.post("/api/users", (req, res)=>{
+    const { email, age } = req.body;
+    const newUser = {
+        email, age, id : Date.now().toString()
+    }
+    users.push(newUser)
+    return res.send(newUser);
+})
+
+
+
+
+// http://localhost:9091/api/users/10003 - GET - Single User
+app.get("/api/users/:userId", (req, res) => {
+    const { userId } = req.params;
+    const foundUser = users.find(user => user.id === userId)
+    if(foundUser){
+        return res.send(foundUser)
+    }else{
+        return res.send({err : "User not found"})
+    }
+})
+
+// http://localhost:9091/api/users - GET - all users
+app.get("/api/users", (req, res)=>{
+    res.send(users);
+})
+
+// http://localhost:9091
+app.get("/", (request, response) => {
+  response.send({ message: "SUCCESS" });
+});
+
+app.listen(9091, () => {
+  console.log("Express started at PORT : 9091");
+});
---
+++ training_ref.md
@@ -0,0 +1,64 @@
+Break : 10:30 - 10:40
+Break : 11:30 - 11:40
+Lunch : 01:00 - 02:00
+Break : 03:00 - 03:10
+Break : 04:00 - 04:10
+
+JavaScript Execution Environment
+- Browser Enviornment (Client Side Code)
+- NodeJS Platform / Server Environment (Server Side Code)
+
+# What is NodeJS?
+- Server Side Platform
+- JavaScript Code / C++ (internally)
+- Performant Apps
+- Single Threaded - IO Non-blocking 
+- Asynchronous
+- Able to handle multiple requests (Queue)
+- Real-time operation
+- Data-intensive
+- Non-blocking
+- Socket / Network programming
+- Based on V8 Engine
+- REPL - Read Evaluate Print Loop
+
+# Node Installer
+- Node Runtime Environment
+- Node Package Manager (NPM)
+- Node Native / Core Module
+
+# To generate package.json 
+- npm init
+
+
+
+
+
+
+# App Versioning - Semantic Versioning - X.Y.Z (semver.org)
+# NoteApp -> 1.0.0
+X -> Major - (2.0.0) New features added without backward compatiblity
+Y -> Minor - (1.1.0) New features introduced without changing existing API (Backward compatible)
+Z -> Patch - 1.0.1
+
+> npm init
+> npm install [-g]
+> npm unintsall
+> npm outdated
+> npm update
+> npm ls
+> npm config get/set
+> npm adduser
+> npm publish
+
+
+# Event System - Stream & Buffer
+# Express 
+# Data persistence using MongoDB
+
+
+
+# buffer size
+
+
+# npm install nodemon -g
