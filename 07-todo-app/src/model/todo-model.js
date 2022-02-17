1c116742eaf51488ed8a2fc2a084db8a61b90192
Sumit K <synergy2411@outlook.com>
2022-02-17 17:16:18 +0530

    hooks added

--- 07-todo-app/src/model/todo-model.js
+++ 07-todo-app/src/model/todo-model.js
@@ -3,12 +3,34 @@ const { Schema, model } = require("mongoose");
 const todoSchema = new Schema({
     label : {
         type : String,
+        validate : {
+            validator : (value) => {
+                // return value.indexOf('@') >= 0
+                return true;
+            },
+            message : "Should have @ character"
+        },
         required : true
     },
     status : {
         type : Boolean,
         default : false
-    }
+    },
+    // age : {
+    //     type : Number,
+    //     max : [60, "Too old to hit API"]
+    // }
+})
+
+todoSchema.pre("save", () => {
+    console.log("Before Storing the Data")
+})
+todoSchema.post("save", () => {
+    console.log("After Storing the Data")
+})
+
+todoSchema.method("myMethod", ()=>{
+    console.log("My Methods to execute")
 })
 
 const TodoModel = model("Todo", todoSchema);
