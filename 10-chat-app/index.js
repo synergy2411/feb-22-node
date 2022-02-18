ba4e709680b1d07410d287ab3070dfed0bc75dbb
Sumit K <synergy2411@outlook.com>
2022-02-18 14:20:50 +0530

    chattr app added

--- 10-chat-app/index.js
+++ 10-chat-app/index.js
@@ -8,6 +8,11 @@ const io = require("socket.io")(server);
 io.on("connection", client => {
     console.log("Client Connected");
     client.emit("acknowledge", {message : "You are now connected!"})
+    client.on("MsgToServer", (chatterName, message) => {
+        console.log(chatterName + " Says : ", message);
+        client.emit("MsgToClient", "Me", message);
+        client.broadcast.emit("MsgToClient", chatterName, message);
+    })
 })
 
 app.get("/", (req, res) => {
